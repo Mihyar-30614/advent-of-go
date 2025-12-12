f29fd990195069b51606c28c8810658b53e2aa5e
Mihyar Al Masalma <eng.mihyear@gmail.com>
2025-12-12 12:56:40 -0400

    Day03 Solved

--- day03/wordfreq.go
+++ day03/wordfreq.go
@@ -1,8 +1,14 @@
 package wordfreq
 
-import "errors"
+import (
+	"errors"
+	"regexp"
+	"sort"
+	"strings"
+)
 
 var ErrInvalidN = errors.New("invalid n")
+var re = regexp.MustCompile(`[a-z0-9]+`)
 
 type WordCount struct {
 	Word  string
@@ -10,16 +16,46 @@ type WordCount struct {
 }
 
 func Tokenize(text string) []string {
-	// TODO: implement
-	return nil
+	lowercaseText := strings.ToLower(text)
+	words := re.FindAllString(lowercaseText, -1)
+
+	if words == nil {
+		return []string{}
+	}
+
+	return words
 }
 
 func CountWords(words []string) map[string]int {
-	// TODO: implement
-	return nil
+	freq := make(map[string]int)
+	for _, word := range words {
+		freq[word]++
+	}
+	return freq
 }
 
 func TopN(counts map[string]int, n int) ([]WordCount, error) {
-	// TODO: implement
-	return nil, nil
+	if n < 0 {
+		return nil, ErrInvalidN
+	}
+
+	// var list []WordCount
+	list := make([]WordCount, 0, len(counts))
+	for w, c := range counts {
+		list = append(list, WordCount{Word: w, Count: c})
+	}
+
+	sort.Slice(list, func(i, j int) bool {
+		if list[i].Count == list[j].Count {
+			return list[i].Word < list[j].Word
+		}
+
+		return list[i].Count > list[j].Count
+	})
+
+	if n > len(list) {
+		n = len(list)
+	}
+
+	return list[:n], nil
 }
